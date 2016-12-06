95fba1905bac002303e516512efdbfc99efeef75
oscarotero <oom@oscarotero.com>
2016-12-06 16:02:39 +0100

    updated tests

--- tests/GeolocationTest.php
+++ tests/GeolocationTest.php
@@ -4,26 +4,19 @@ namespace Middlewares\Tests;
 
 use Middlewares\Geolocation;
 use Middlewares\Utils\Dispatcher;
-use Middlewares\Utils\CallableMiddleware;
-use Zend\Diactoros\ServerRequest;
-use Zend\Diactoros\Response;
+use Middlewares\Utils\Factory;
 
 class GeolocationTest extends \PHPUnit_Framework_TestCase
 {
     public function testGeolocation()
     {
-        $request = new ServerRequest(['REMOTE_ADDR' => '123.9.34.23']);
+        $request = Factory::createServerRequest(['REMOTE_ADDR' => '123.9.34.23']);
 
         $response = (new Dispatcher([
             new Geolocation(),
-            new CallableMiddleware(function ($request) {
-                $address = $request->getAttribute('client-location');
-
-                $response = new Response();
-                $response->getBody()->write($address->first()->getCountry());
-
-                return $response;
-            }),
+            function ($request) {
+                echo $request->getAttribute('client-location')->first()->getCountry();
+            },
         ]))->dispatch($request);
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
